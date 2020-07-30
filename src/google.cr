55bc63d27ec91494c0ca16d880dc387587a9fcce
Amit Gaur <amitgaur.web@gmail.com>
2020-07-30 16:08:01 +1000

    google pay pass creation support

--- src/google.cr
+++ src/google.cr
@@ -2,6 +2,7 @@ require "./calendar/calendar"
 require "./directory/directory"
 require "./files/files"
 require "./translate/translate"
+require "./passes/event_tickets"
 
 module Google
   GOOGLE_URI = URI.parse("https://www.googleapis.com")

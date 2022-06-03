f4bffb63304a4ddc4e729929d3b54acf0ce625a0
Mia Bennett <mia@chillfox.com>
2022-06-03 11:32:49 +0930

    refactor(firebase): change int64 converter to a generic converter
    Co-authored-by: Caspian Baska <email@caspian.computer>

--- src/firebase/firebase_int64_converter.cr
+++ src/firebase/firebase_int64_converter.cr
@@ -1,11 +1,12 @@
 module Google
   class FirebaseAuth
-    module Int64Converter
-      def self.from_json(value : JSON::PullParser) : Int64
-        value.read_string.to_i64
+    # Converter for stringly typed values
+    module StringTypeConverter(T)
+      def self.from_json(json : JSON::PullParser) : T
+        T.new(json.read_string)
       end
 
-      def self.to_json(value : Int64, json : JSON::Builder) : Nil
+      def self.to_json(value : T, json : JSON::Builder)
         json.string(value.to_s)
       end
     end
